dfba86fa1d5993ab9f53aaca5565d14f7d1f4b84
Carlos Caballero <carlos8caballero@gmail.com>
2024-06-23 00:38:10 -0700

    draw line

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from window import Window
+from window import Line, Point, Window
 
 def main():
     win = Window(800, 600)
+    line1 = Line(Point(10,100), Point(200,300))
+    win.draw_line(line1,"green")
     win.wait_for_close()
 
 if __name__ == "__main__":
--- window.py
+++ window.py
@@ -1,5 +1,18 @@
 from tkinter import Tk, BOTH, Canvas
 
+class Point():
+    def __init__(self, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
+
+class Line():
+    def __init__(self, start: Point, end: Point) -> None:
+        self.p1 = start
+        self.p2 = end
+
+    def draw(self, canvas: Canvas, fill_color: str="black" ) -> None:
+        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
+
 class Window():
     def __init__(self, width: int, height: int) -> None:
         self.__root = Tk()
@@ -20,3 +33,6 @@ class Window():
 
     def close(self) -> None:
         self.__running = False
+
+    def draw_line(self, line: Line, fill_color:str="black") -> None:
+        line.draw(self.__canvas, fill_color)
